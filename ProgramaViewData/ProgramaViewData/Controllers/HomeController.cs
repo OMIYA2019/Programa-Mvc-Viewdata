68841b7e0250718a4032ebd94b997626cd7c6a15
omiya takakazu <omiya2015@gmail.com>
2020-04-01 20:53:01 -0300

    Programa Mvc Viewdata

---
+++ ProgramaViewData/ProgramaViewData/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using ProgramaViewData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgramaViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cliente()
+        {
+            Cliente cliente = new Cliente()
+            {
+                cliId = 23,
+                cliNome = "Gizcreuza",
+                cliEnd = "Rua Tá Difícil, 00",
+                cliNasc = "23/05/1996 00:00:00"
+            };
+            ViewData["cliId"] = cliente.cliId;
+            ViewData["cliNome"] = cliente.cliNome;
+            ViewData["cliEnd"] = cliente.cliEnd;
+            ViewData["cliNasc"] = cliente.cliNasc;
+            return View();
+        }
+
+        public ActionResult Produto()
+        {
+            Produto produto = new Produto()
+            {
+                prodId = 1,
+                prodNome = "Bom",
+                prodValor = 149.99m,
+                prodQtd = 100
+            };
+            ViewData["prodId"] = produto.prodId;
+            ViewData["prodNome"] = produto.prodNome;
+            ViewData["prodValor"] = produto.prodValor;
+            ViewData["prodQtd"] = produto.prodQtd;
+            return View();
+        }
+
+        public ActionResult Venda()
+        {
+            Venda venda = new Venda()
+            {
+                vendaId = 100,
+                vendaData = "28/02/2019 00:00:00",
+                vendaTotal = 500m
+
+            };
+            ViewData["vendaId"] = venda.vendaId;
+            ViewData["vendaData"] = venda.vendaData;
+            ViewData["vendaTotal"] = venda.vendaTotal;
+            return View();
+        }
+
+    }
+}
+
---
+++ ProgramaViewData/ProgramaViewData/Models/Venda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgramaViewData.Models
+{
+    public class Venda
+    {
+        public int vendaId { get; set; }
+        public string vendaData { get; set; }
+        public decimal vendaTotal { get; set; }
+    }
+}
